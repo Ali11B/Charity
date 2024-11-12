c9ad14419ccef241fff9b4f4409df875ad5d48c7
Ali Bahrami <A.Bahrami80@gmail.com>
2024-11-12 10:44:18 +0330

    adding model names!

--- charity/accounts/models.py
+++ charity/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Benefactor (models.Model):
+    experience = models.SmallIntegerField(default=0)
+
+class Charity (models.Moedel):
+    name = models.CharField(max_length=50)
+
+
+
+class Task (models.Moodel):
+    assigned_benefactor = models.ForeignKey(Benefactor , on_delete= models.SET_NULL)
+    charity = models.ForeignKey(Charity,on_delete=models.CASCADE)
+    age_limit_from = models.IntegerField()
+    age_limit_to = models.IntegerField()
+
