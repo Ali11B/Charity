f4a10341033c103bff38007b03ec4f62574b5e6a
Ali Bahrami <A.Bahrami80@gmail.com>
2024-11-13 11:23:24 +0330

    models update

--- charity/accounts/models.py
+++ charity/accounts/models.py
@@ -9,8 +9,21 @@ class Charity (models.Moedel):
 
 
 class Task (models.Moodel):
+    state_LOV = (
+                    ('P' , 'Pending'),
+                    ('W' , 'Waiting'),
+                    ('A' , 'Assigned'),
+                    ('D' , 'Done') )
+    gender_LOV = (
+        ('F','F'),
+        ('M' , 'M')
+    )
+    
     assigned_benefactor = models.ForeignKey(Benefactor , on_delete= models.SET_NULL)
-    charity = models.ForeignKey(Charity,on_delete=models.CASCADE)
+    charity = models.ForeignKey(Charity)
     age_limit_from = models.IntegerField()
     age_limit_to = models.IntegerField()
-
+    date = models.DateField()
+    state = models.CharField(max_length=10 , choices=state_LOV)
+    title = models.CharField(max_length= 60)
+    gender_limit = models.CharField(max_length=1 , choices= gender_LOV)
