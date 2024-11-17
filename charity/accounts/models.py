88e687579afd5331ca458b53dcaaf8fa16995ee1
Ali Bahrami <A.Bahrami80@gmail.com>
2024-11-17 10:47:07 +0330

    changes on models

--- charity/accounts/models.py
+++ charity/accounts/models.py
@@ -1,29 +1,11 @@
 from django.db import models
-
-class Benefactor (models.Model):
-    experience = models.SmallIntegerField(default=0)
-
-class Charity (models.Moedel):
-    name = models.CharField(max_length=50)
+from django.contrib.auth.models import AbstractUser
 
 
-
-class Task (models.Moodel):
-    state_LOV = (
-                    ('P' , 'Pending'),
-                    ('W' , 'Waiting'),
-                    ('A' , 'Assigned'),
-                    ('D' , 'Done') )
-    gender_LOV = (
-        ('F','F'),
-        ('M' , 'M')
-    )
-    
-    assigned_benefactor = models.ForeignKey(Benefactor , on_delete= models.SET_NULL)
-    charity = models.ForeignKey(Charity)
-    age_limit_from = models.IntegerField()
-    age_limit_to = models.IntegerField()
-    date = models.DateField()
-    state = models.CharField(max_length=10 , choices=state_LOV)
-    title = models.CharField(max_length= 60)
-    gender_limit = models.CharField(max_length=1 , choices= gender_LOV)
+class User(AbstractUser) : 
+    Gender_LOV = (('F','F') , ('M' , 'M'))
+    address = models.TextField(blank=True, null=True)
+    age = models.PositiveSmallIntegerField(blank=True , null= True)
+    description = models.TextField(blank=True , null = True)
+    gender = models.CharField(max_length= 1 , blank= True , null =True , choices= Gender_LOV)
+    phone = models.CharField(max_length= 15 , blank= True , null= True)
